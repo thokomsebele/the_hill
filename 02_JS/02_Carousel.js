492954daad5fb197d533e627a19d139cf0bcb746
Thokozani Msebele <thoko@byosoftware.co.zw>
2024-08-15 02:10:53 +0200

    footer media queries sorted

--- 02_JS/02_Carousel.js
+++ 02_JS/02_Carousel.js
@@ -1,38 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
+  let currentIndex = 0;
   const container = document.getElementById("carouselContainer");
   const faces = container.querySelectorAll(".carouselFace");
   const totalFaces = faces.length;
-  let currentIndex = 0;
-  let intervalId; // Variable to hold the interval ID for auto-rotation
   function goToFace(index) {
-    // Calculate translateX value based on index
     const translateValue = `translateX(${-index * 100}%)`;
     container.style.transform = translateValue;
     currentIndex = index;
   }
-  function startAutoRotate() {
-    // intervalId = setInterval(() => {
-    //   goToFace(currentIndex < totalFaces - 1 ? currentIndex + 1 : 0);
-    // }, 3000); // Change interval duration as needed (3000ms = 3 seconds)
-  }
-  function stopAutoRotate() {
-    clearInterval(intervalId);
-  }
-  // Start auto-rotate when the carousel container is in view
-  function checkCarouselInView() {
-    const rect = container.getBoundingClientRect();
-    const isVisible =
-      rect.top >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight);
-    if (isVisible) startAutoRotate();
-    else stopAutoRotate();
-  }
-  // Initial check when the page loads
-  checkCarouselInView();
-  // Check periodically when scrolling
-  window.addEventListener("scroll", checkCarouselInView);
-  // Event listeners for manual navigation
   document
     .getElementById("arrowDivLeft")
     .addEventListener("click", function () {
@@ -48,21 +23,23 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  if (window.innerWidth <= 999) {
-    let contArr = document.querySelectorAll(".carouselFaceImageDiv");
-    for (let cInd = 0; cInd < contArr.length; cInd++) {
-      let cont = contArr[cInd];
-      let img = cont.querySelector("img");
-      let scrollStep = 2; // Scroll speed
-      let maxScroll = img.offsetWidth - window.innerWidth;
-      let scrollPosition = 0;
-      scrollImage();
-      function scrollImage() {
-        scrollPosition += scrollStep;
-        if (scrollPosition > maxScroll) scrollPosition = 0;
-        cont.scrollLeft = scrollPosition;
-        requestAnimationFrame(scrollImage);
+  setInterval(function () {
+    if (window.innerWidth <= 999) {
+      let contArr = document.querySelectorAll(".carouselFaceImageDiv");
+      for (let cInd = 0; cInd < contArr.length; cInd++) {
+        let cont = contArr[cInd];
+        let img = cont.querySelector("img");
+        let scrollStep = 2; // Scroll speed
+        let maxScroll = img.offsetWidth - window.innerWidth;
+        let scrollPosition = 0;
+        scrollImage();
+        function scrollImage() {
+          scrollPosition += scrollStep;
+          if (scrollPosition > maxScroll) scrollPosition = 0;
+          cont.scrollLeft = scrollPosition;
+          requestAnimationFrame(scrollImage);
+        }
       }
     }
-  }
+  }, 2500);
 });
--- 02_JS/07_Updates.js
+++ 02_JS/07_Updates.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let contColFirst = document.querySelector(".updatesContainer_Col");
-  console.log("contColFirst : contColFirst");
-  console.log(contColFirst);
-  let clsX = "updatesContainer_Col_Min";
-  if (window.innerWidth <= 749) contColFirst.classList.add(clsX);
-  else contColFirst.classList.remove(clsX);
+  setInterval(function () {
+    let contColFirst = document.querySelector(".updatesContainer_Col");
+    let clsX = "updatesContainer_Col_Min";
+    if (window.innerWidth <= 749) contColFirst.classList.add(clsX);
+    else contColFirst.classList.remove(clsX);
+  }, 2500);
 });
