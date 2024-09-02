a638ecba4d944ca7162c134f2d1e6e215c292e71
Thokozani Msebele <thoko@byosoftware.co.zw>
2024-09-03 00:34:16 +0200

    wip curriculum

--- gulpVariables/pagesArray.js
+++ gulpVariables/pagesArray.js
@@ -23,4 +23,12 @@ module.exports = [
     outputCSS: "2_about.css",
     pageSections: ["11_Content", "08_Footer"],
   },
+  {
+    pageName: "curriculum",
+    metaDataPageDescription: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean congue turpis id tellus vestibulum, sit amet vulputate ante finibus. Aenean lacinia nisl sapien, ut molestie sapien elementum sed. In fringilla aliquam tortor, quis lobortis purus sollicitudin quis. In hac habitasse platea dictumst. Nullam congue tempus sapien ut laoreet. Donec quis odio eget lacus dictum pulvinar. Aliquam iaculis odio nec nibh hendrerit posuere. Nunc nec commodo sem, et faucibus libero.`,
+    templatePage: "3_curriculum.njk",
+    outputHTML: "curriculum.html",
+    outputCSS: "3_curriculum.css",
+    pageSections: ["10_Video_Curriculum", "12_Subjects", "08_Footer"],
+  },
 ];
