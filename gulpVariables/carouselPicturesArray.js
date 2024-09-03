4b1c8b09733691322a3591aede40fa17cf4e510e
Thokozani Msebele <thoko@byosoftware.co.zw>
2024-09-03 15:59:04 +0200

    carouseld done

--- gulpVariables/carouselPicturesArray.js
+++ gulpVariables/carouselPicturesArray.js
@@ -1,9 +1,32 @@
 module.exports = [
-  "c01.jpg",
-  "c02.jpg",
-  "c03.jpg",
-  "c04.jpg",
-  "c05.jpg",
-  "c06.jpg",
-  "c07.jpg",
+  {
+    image: "c01.jpg",
+    caption_superscript: `Welcome to`,
+    caption_header: `The Hill Preparatory School`,
+    caption_subscript: `We believe in holistic child development that turns every student into an active global citizen.`,
+  },
+  {
+    image: "c02.jpg",
+    caption_superscript: ``,
+    caption_header: `Our School`,
+    caption_subscript: `A private, co-educational, day school focusing on holistic child development.`,
+  },
+  {
+    image: "c03.jpg",
+    caption_superscript: ``,
+    caption_header: `Our Curriculum`,
+    caption_subscript: `We offer the Oxford International Curriculum that covers; Academics, Well-Being, Computing, Practical Global Life- Skills & Sustainability.`,
+  },
+  {
+    image: "c04.jpg",
+    caption_superscript: ``,
+    caption_header: `Sports`,
+    caption_subscript: `Our sporting curriculum fosters teamwork, resilience and fitness in every Child. We are equipped to train sporting talent. `,
+  },
+  {
+    image: "c05.jpg",
+    caption_superscript: ``,
+    caption_header: `Art and Culture`,
+    caption_subscript: `We appreciate the diversity of talent and offer a robust arts and culture curriculum designed to identify and nurture budding talent.`,
+  },
 ];
